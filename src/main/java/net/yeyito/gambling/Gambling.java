9e38510aada031269f3eb624800ef66f95f7bf89
Yeyito777 <alinerob31@gmail.com>
2024-07-26 17:50:43 -0500

    Basic functionality

--- src/main/java/net/yeyito/gambling/Gambling.java
+++ src/main/java/net/yeyito/gambling/Gambling.java
@@ -1,13 +1,28 @@
 package net.yeyito.gambling;
 
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Gambling extends JavaPlugin {
+    private static Plugin instance;
+    private static World OVERWORLD;
+
+    public static World getOverworld() {
+        return OVERWORLD;
+    }
+
+    public static Plugin getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("Gambling enabled!");
+        OVERWORLD = this.getServer().getWorlds().get(0);
+        Server server = this.getServer();
     }
 
     @Override
---
+++ src/main/java/net/yeyito/gambling/util/Chair.java
@@ -0,0 +1,10 @@
+package net.yeyito.gambling.util;
+
+import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
+
+public class Chair {
+    public static void SummonChair(Location location, BlockFace facing) {
+
+    }
+}
