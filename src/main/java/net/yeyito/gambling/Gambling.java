4f3b5c574f70d99149e30b41dc4437189ca6d480
Yeyito777 <alinerob31@gmail.com>
2024-07-26 17:42:38 -0500

    Init commit

---
+++ src/main/java/net/yeyito/gambling/Gambling.java
@@ -0,0 +1,17 @@
+package net.yeyito.gambling;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gambling extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
