18c0584a1ce2aa6aa051a335ebfebdc3a49dc163
Yeyito777 <alinerob31@gmail.com>
2024-07-26 18:26:52 -0500

    Chair functionality

--- src/main/java/net/yeyito/gambling/Gambling.java
+++ src/main/java/net/yeyito/gambling/Gambling.java
@@ -1,10 +1,36 @@
 package net.yeyito.gambling;
 
-import org.bukkit.Server;
-import org.bukkit.World;
+import net.yeyito.gambling.util.Chair;
+import org.bukkit.*;
+import org.bukkit.block.BlockFace;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import io.papermc.paper.command.brigadier.argument.resolvers.ArgumentResolver;
+import io.papermc.paper.math.Position;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.entity.DragonFireball;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.ScoreboardManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/** @noinspection UnstableApiUsage*/
 public final class Gambling extends JavaPlugin {
     private static Plugin instance;
     private static World OVERWORLD;
@@ -23,10 +49,36 @@ public final class Gambling extends JavaPlugin {
         getLogger().info("Gambling enabled!");
         OVERWORLD = this.getServer().getWorlds().get(0);
         Server server = this.getServer();
+        server.getPluginManager().registerEvents(new Chair(),this);
+
+        LifecycleEventManager<Plugin> manager = this.getLifecycleManager();
+        manager.registerEventHandler(LifecycleEvents.COMMANDS, event -> {
+            final Commands commands = event.registrar();
+            commands.register(
+                    Commands.literal("chair")
+                            .requires(sender -> sender.getSender().hasPermission("chair.summon"))
+                            .executes(ctx -> summonChair(ctx))
+                            .build(),
+                    "Summons a chair at the player's location",
+                    List.of("sitdown")
+            );
+        });
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private int summonChair(CommandContext<CommandSourceStack> ctx) {
+        Location playerLocation = ((Player) ctx.getSource().getSender()).getLocation();
+        BlockFace playerFacing = ((Player) ctx.getSource().getSender()).getFacing();
+
+        // Adjust the location to be in front of the player
+        Location chairLocation = playerLocation.add(playerFacing.getDirection());
+
+        Chair.summonChair(chairLocation, playerFacing);
+        return Command.SINGLE_SUCCESS;
     }
 }
--- src/main/java/net/yeyito/gambling/util/Chair.java
+++ src/main/java/net/yeyito/gambling/util/Chair.java
@@ -1,10 +1,48 @@
 package net.yeyito.gambling.util;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Directional;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Interaction;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
-public class Chair {
-    public static void SummonChair(Location location, BlockFace facing) {
+public class Chair implements Listener {
 
+    public static void summonChair(Location location, BlockFace facing) {
+        // Set the chair block
+        Block chairBlock = location.getBlock();
+        chairBlock.setType(Material.OAK_STAIRS);
+
+        // Set the facing direction of the stairs
+        Directional blockData = (Directional) chairBlock.getBlockData();
+        blockData.setFacing(facing);
+        chairBlock.setBlockData(blockData);
+
+        // Spawn the interaction entity
+        Location interactionLoc = chairBlock.getLocation().add(0.5, 0.5, 0.5);
+        Interaction interaction = (Interaction) interactionLoc.getWorld().spawnEntity(interactionLoc, EntityType.INTERACTION);
+        interaction.setInteractionWidth(0.6f);
+        interaction.setInteractionHeight(0.6f);
+    }
+
+    @EventHandler
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        if (event.getRightClicked() instanceof Interaction) {
+            Interaction interaction = (Interaction) event.getRightClicked();
+            Player player = event.getPlayer();
+
+            // Check if the interaction entity is above a stairs block
+            Block block = interaction.getLocation().subtract(0, 0, 0).getBlock();
+            if (block.getType() == Material.OAK_STAIRS && interaction.getPassengers().size() < 1) {
+                // Make the player sit (ride) the interaction entity
+                interaction.addPassenger(player);
+            }
+        }
     }
 }
